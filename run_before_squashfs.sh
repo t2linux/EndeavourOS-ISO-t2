895a6c3e128fb1d0bfb3e57b76d7bf9d051a79cc
Noa Himesaka <himesaka@noa.codes>
2023-10-24 16:04:11 +0900

    Add bolt and fix theming

--- run_before_squashfs.sh
+++ run_before_squashfs.sh
@@ -116,6 +116,9 @@ pacman -Qs | grep "/xorg-server " | cut -c7- >> iso_package_versions
 pacman -Qs | grep "/nvidia-utils " | cut -c7- >> iso_package_versions
 mv "iso_package_versions" "/home/liveuser/"
 
+# Set theme
+lookandfeeltool -a com.endeavouros.breezedarkeos.desktop
+
 echo "############################"
 echo "# end chrooted commandlist #"
 echo "############################"
