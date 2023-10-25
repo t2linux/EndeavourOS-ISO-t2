aa70f6c7f9f96bca7ff54ffa904c152562d8a335
Noa Himesaka <himesaka@noa.codes>
2023-10-25 11:17:15 +0900

    Properly set wallpaper

--- airootfs/root/endeavouros-skel-liveuser/src/etc/skel/.theming.sh
+++ airootfs/root/endeavouros-skel-liveuser/src/etc/skel/.theming.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+
+# Set theme to Breeze Dark EOS
 lookandfeeltool -a com.endeavouros.breezedarkeos.desktop
+
+# Set wallpaper to /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png
+qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
+var allDesktops = desktops();
+for (i=0;i<allDesktops.length;i++) {
+    d = allDesktops[i];
+    d.wallpaperPlugin = "org.kde.image";
+    d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");
+    d.writeConfig("Image", "file:///usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png")
+}
+'
+
