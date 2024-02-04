d7ba49bcace4c8d1996709663498d96d43044dac
Noa Himesaka <himesaka@noa.codes>
2024-02-04 15:42:44 +0900

    Galileo Neo-T2 release

--- profiledef.sh
+++ profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="EndeavourOS-T2"
-iso_label="EOS_T2_GALILEO_PLUS"
+iso_label="EOS_T2_GALILEO_NEO"
 iso_publisher="t2linux <https://t2linux.org>"
 iso_application="EndeavourOS Live/Rescue CD for Macs with T2 security chip"
-iso_version="Galileo Plus-T2"
+iso_version="Galileo Neo-T2"
 install_dir="arch"
 buildmodes=('iso')
 quiet="n"
