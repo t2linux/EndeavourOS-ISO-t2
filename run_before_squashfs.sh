a1868e31ae2ef859885c9bc3b54fb6a39f892b37
Noa Himesaka <himesaka@noa.codes>
2024-10-11 00:33:15 +0900

    EndeavourOS Endeavour Neo-T2

--- run_before_squashfs.sh
+++ run_before_squashfs.sh
@@ -116,6 +116,10 @@ pacman -Qs | grep "/xorg-server " | cut -c7- >> iso_package_versions
 pacman -Qs | grep "/nvidia-utils " | cut -c7- >> iso_package_versions
 mv "iso_package_versions" "/home/liveuser/"
 
+# use iwd as NetworkManager backend
+echo "[device]" >> /etc/NetworkManager/conf.d/wifi_backend.conf
+echo "wifi.backend=iwd" >> /etc/NetworkManager/conf.d/wifi_backend.conf
+
 echo "############################"
 echo "# end chrooted commandlist #"
 echo "############################"
