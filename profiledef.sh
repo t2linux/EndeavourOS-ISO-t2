23a97b9c65abd27b13aecf1ae9230e4fd92fda1e
Noa Himesaka <himesaka@noa.codes>
2025-12-24 14:28:33 +0900

    EndeavourOS Ganymede-T2

--- profiledef.sh
+++ profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="EndeavourOS-T2"
-iso_label="EOS_T2_ENDEAVOUR_NEO"
+iso_label="EOS_T2_GANYMEDE"
 iso_publisher="t2linux <https://t2linux.org>"
 iso_application="EndeavourOS Live/Rescue CD for Macs with T2 security chip"
-iso_version="Mercury-T2"
+iso_version="Ganymede-T2"
 install_dir="arch"
 buildmodes=('iso')
 quiet="n"
